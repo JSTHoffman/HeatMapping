de674707196d19aef37b8584ce91d3f46131945f
JSTHoffman <jsthoffman90@gmail.com>
2016-07-08 15:49:05 -0700

    Updated AutoFmask.py for daytime L8 images

--- AutoFmask.py
+++ AutoFmask.py
@@ -13,6 +13,8 @@
 # 1) place all TIF files for each landsat scene in separate folders
 # 2) set scenePath to directory containing scene folders
 # 3) make sure no other folders are present in the directory
+# 4) requires the installation of Fmask - set cmd to the Fmask path
+# 5) requires the installation of Matlab Compiler Runtime (MCR)
 
 # --------------------------------
 
@@ -23,7 +25,7 @@ import subprocess, os
 cmd = r'C:\Fmask\Fmask.exe'
 
 # path to folders containing TIFFs for landsat scene
-scenePath = r'C:\Users\Sustainability\Desktop\Summer\LandsatData\FmaskTest'
+scenePath = r'C:\Users\SUST heavyweight\Desktop\LandsatDaytime_BulkDownload\L8_OLI_TIRS'
 
 # loop through items in folder containing all scenes
 for aPath in os.listdir(scenePath):
@@ -36,7 +38,7 @@ for aPath in os.listdir(scenePath):
 
 		process = subprocess.Popen(
 			[cmd],						# command to run
-			cwd = path					# directory to run from 
+			cwd=path					# directory to run from
 		)
 
 		# wait for process to finish
