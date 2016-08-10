540699ffbcb4216faff578c8cce1571334980fcc
JSTHoffman <jsthoffman90@gmail.com>
2016-08-10 16:35:01 -0700

    Updated linear regression script.

--- LinearRegress.py
+++ LinearRegress.py
@@ -1,16 +1,27 @@
 import matplotlib.pyplot as plt
-import pandas as pd
 import numpy as np
+import pandas as pd
 
-df = pd.read_csv(r'C:\Users\SUST heavyweight\Desktop\daymedlst_ndvi.csv')
+# C:\Users\SUST heavyweight\Desktop\daymedlst_ndvi.csv
+df = pd.read_csv(r'T:\JHoffman\Summer\NDVI\Sample_DayLST_NDVI.csv')
+df = df[df.day_medlst > 297.2]
+df = df[df.medNDVI > -0.1]
 
-x = df['GRID_COD_1']
-y = df['GRID_CODE']
+# n = 50
+# x = np.random.randn(n)
+# y = x * np.random.randn(n)
 
+x = np.array(df['day_medlst']) # GRID_CODE
+y = np.array(df['medNDVI']) # GRID_COD_1
+
+plt.rc('font', family='serif', size=13)
 fig, ax = plt.subplots()
-fit = np.polyfit(x.value.flatten(), y.values.flatten, deg=1)
+fit = np.polyfit(x, y, deg=1)
 ax.plot(x, fit[0] * x + fit[1], color='red')
 ax.scatter(x, y)
 
-print fig
-print fit
+r = np.corrcoef(x, y)[0, 1]
+print 'R^2 = {0}'.format(r**2)
+plt.show()
+
+print '\nEnd of script.'
